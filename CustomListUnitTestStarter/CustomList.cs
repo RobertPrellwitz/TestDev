b2124431f65372b40396304470d6ff6b2d29674c
Rob Prellwitz <robprellwitz@uwalumni.com>
2020-10-26 18:01:01 -0500

    Properties Count and Capacity setup

--- CustomListUnitTestStarter/CustomList.cs
+++ CustomListUnitTestStarter/CustomList.cs
@@ -8,6 +8,31 @@ namespace CustomListUnitTestStarter
 {
     public class CustomList<T>
     {
+        private int count = 0;
+        public int Count
+        {
+            get => count;
+            set
+            {
+                count = value;
+            }
+        }
+        private int capacity = 4;
+        public int Capacity
+        {
+            get => capacity;
+            set
+            {
+                if (Count > capacity)
+                {
+                    capacity = capacity * 2;
+                }
+            }
+        }
+
+        // member varialbe
+
+        T[] items;
         public void Add(T item)
         {
 
